305cc2f8596f80f91c29bb16cdc9a8a789e927d7
Kliver Daniel <kliver1998@gmail.com>
2021-05-24 16:28:13 -0500

    create structure to tests

---
+++ test/contract/specs/list-user.contract.test.js
@@ -0,0 +1,43 @@
+import { provider } from '../config/init-pact';
+import { UserController as controller } from '../../../src/modules/UserController';
+import { Matchers } from '@pact-foundation/pact';
+
+describe('Given an user service', () => {
+    beforeAll(async() => {
+        await provider.setup();
+    });
+
+    describe('When a request to list all the users is made', () => {
+        beforeAll(async() => {
+            await provider.addInteraction({
+                state: 'has users and want to list them',
+                uponReceiving: 'a request to list all users',
+                withRequest: {
+                    method: 'GET',
+                    path: '/users'
+                },
+                willRespondWith: {
+                    status: 200,
+                    body: Matchers.eachLike({
+                        name: Matchers.string('Manchas'),
+                        breed: Matchers.like("Bengali"),
+                        gender: Matchers.like("Female"),
+                        vaccinated: Matchers.boolean(true),
+                    }, {min: 1})
+                }
+            });
+        });
+
+        test('Then it should return the right data', async() => {
+            const response = await controller.list();
+            expect(response.data).toMatchSnapshot();
+
+            await provider.verify();
+        })
+
+    });
+
+    afterAll(async () => {
+        await provider.finalize();
+    });
+});
